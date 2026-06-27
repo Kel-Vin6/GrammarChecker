a4e264bc55fc59777bd4ba66793da4217e145ae8
Kel-Vin6 <bron28240@gmail.com>
2026-06-27 01:51:33 +0000

    Initial Commit

---
+++ GrammarChecker/GrammarCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammarCheckerApp.swift
+//  GrammarChecker
+//
+//  Created by Kelvin Wallace on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GrammarCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
